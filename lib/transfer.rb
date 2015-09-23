be19a9c59e3b622cd81c634cb0ccc5cc18774917
allensoberano <allensoberano@gmail.com>
2015-09-23 13:51:41 -0700

    transfer complete

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -40,12 +40,10 @@ class Transfer
 
 
   def execute_transaction
-    if sender.balance > amount
-      if @status == 'pending'
+    if sender.balance > amount && @status == 'pending'
         @sender.balance -= amount
         @receiver.balance += amount
         @status = "complete"
-      end
     else
       @status = 'rejected'
       "Transaction rejected. Please check your account balance."
