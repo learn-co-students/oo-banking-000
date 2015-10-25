a9d8d5f7d2729f4921f4f2d7be1eb14e9d457946
NoneOfMaster <rcraigt@gmail.com>
2015-10-25 16:39:55 -0700

    funny banking pun

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,42 @@
+require 'pry'
+
 class Transfer
-  # code here
+
+  attr_reader :sender, :receiver, :amount
+  attr_accessor :status
+
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver  
+    @status = "pending"
+    @amount = amount
+  end
+
+  def both_valid?
+    if sender.valid? && receiver.valid?
+      true
+    else
+      false
+    end
+  end
+
+  def execute_transaction
+    if self.both_valid? && self.status == "pending" && sender.balance > @amount
+      sender.deposit(-@amount)
+      receiver.deposit(@amount)
+      self.status = "complete"
+    else
+      self.status = "rejected"
+      "Transaction rejected. Please check your account balance."
+    end
+  end
+
+  def reverse_transfer
+    if self.status == "complete"
+      sender.deposit(@amount)
+      receiver.deposit(-@amount)
+      self.status = "reversed"
+    end
+  end
+
 end
